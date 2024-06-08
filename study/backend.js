537a4aa0ec30616dcdba767cbd9c9cd3bb5713bd
Hiro Yama <hiroo@yamweb.com>
2024-06-08 14:04:47 +0900

    test

--- study/backend.js
+++ study/backend.js
@@ -1,3 +1,5 @@
+
+
 // express setup
 const express = require("express");
 const app = express();
@@ -5,6 +7,7 @@ const app = express();
 // socket.io setup
 const http = require("http");
 const server = http.createServer(app);
+
 const { Server } = require("socket.io");
 const io = new Server(server);
 
@@ -16,16 +19,13 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-
 io.on('connection', (socket) => {
   console.log('a user connected: ' + socket.id)
   io.emit('update')
 })
 
-
 server.listen(port, () => {
   console.log(`Im listening on port ${port}`);
 });
 
-
-console.log("server did load 003");
+console.log("server loaded.");
